24fb557c2de1c52566e5bfd710848cbeb76d66b0
KPolrolniczak <k.polrolniczak@gmail.com>
2022-03-29 13:59:13 +0200

    Added drafts of the remaining tests (functions to be finished)

--- cypress/integration/photo-displayer-ui-tests.js
+++ cypress/integration/photo-displayer-ui-tests.js
@@ -7,6 +7,45 @@ describe ("photoDisplayUITests", () => {
         displaysThreeImages()
     })
 
+    it ("after clicking on Next button, three new pictures are generated", () => {
+        //given
+        websiteIsOpened()
+
+        //when
+        //zapisuje do tablicy urle starych img
+        let srcGetter = cy.get('.img-responsive').invoke('attr', 'src')
+            .then((el) => {
+                debugger
+            });
+        cy.log(srcGetter)
+        clickedNextBtn()
+
+        //then
+        // displaysThreeNewImages()
+        //zapisuje do nowej tablicy urle nowych img
+        //porównuję dwie tablice i oczekuje, ze url będą różne
+    })
+
+    it ("after clicking on Prev button, three previous pictures are generated", () => {
+        //given
+        websiteIsOpened()
+
+        //when
+        clickedNextBtn()
+
+        //then
+        displaysThreeNewImages()
+    })
+
+    it ("while page is loaded for the first time, only Next button is visible", () => {
+        //given
+        websiteIsOpened()
+
+        //then
+        noPrevBtn()
+    })
+
+    it ("while there is no more pictures to generate, only Prev button is visible",
 
 })
 
